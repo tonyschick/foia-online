3a6a2bd8c076deb40941087d066103abc84e62ab
Tony Schick <aschick@opb.org>
2016-05-09 16:20:36 -0700

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,10 +1,10 @@
-import time, datetime, urllib3, re, requests, os, csv
+import time, datetime, urllib3, re, requests, os, csv, sys
 # import mechanize
 import time, datetime
 from bs4 import BeautifulSoup
 
 #disable warnings
-#urllib3.disable_warnings()
+urllib3.disable_warnings()
 
 # Create a CSV to log the work
 # Write in a first row with the following labels, to become our column headers
@@ -16,27 +16,24 @@ with open('foia-download-log.csv', 'a', newline='') as logfile:
 
 http = urllib3.PoolManager()
 
-# Ex. https://foiaonline.regulations.gov/foia/action/public/view/request?objectId=090004d280c519ce
-foia_online_id = input("Copy and paste the ID for your FOIA (the numbers after objectId= ) ")
+foia_online_id = sys.argv[1]
+url = "https://foiaonline.regulations.gov/foia/action/public/view/request?objectId={}".format(foia_online_id)
 
-url = foia_online_id
+# using urllib3 to open the page
+html = http.urlopen('GET',url)
 
-try:
-    # using urllib3 to open the page
-    html = http.urlopen('GET',url)
-    # and to read the html data from the request
-    html = html.data
-    # feed it into Beautiful Soup, our html parsing library
-    bs = BeautifulSoup(html, "lxml")
+# and to read the html data from the request
+html = html.data
 
-    #find how many pages there are holding our documents. Divide by 2 because they're listed twice, at the top and bottom of the page
-    number_of_pages = len(bs.findAll(True, {"class": "resPagNum"})) / 2
-    number_of_pages = int(number_of_pages)
+# feed it into Beautiful Soup, our html parsing library
+bs = BeautifulSoup(html, "lxml")
 
-    print(number_of_pages)
-except:
-    print("Error!")
+#find how many pages there are holding our documents. Obtained by grabbing the page number out of the url in the "last page"
+number_of_pages = bs.find_all("a", {"class": "resPagNav"})[3].get('href')
+number_of_pages = re.findall('(?<=p=)(.*?)(?=#dttPubRecords)', number_of_pages)
 
+number_of_pages = int(number_of_pages[0])
+print("Total pages: {}".format(number_of_pages))
 
 counter = 0
 # iterate through a list of numbers, add 1 to number_of_pages so list doesn't stop short
@@ -44,6 +41,7 @@ for d in range(1,(number_of_pages+1)):
 
     # open a url, changing the 'p=' for each number in our list, so that it opens all pages 1-5, in order
     page_url = "https://foiaonline.regulations.gov/foia/action/public/view/request?event=request&objectId=090004d280b6c05e&d-8138531-p={}#dttPubRecords".format(d)
+    # print(page_url)
 
     #open the page_url and pull the html out of it and into a variable
     page_html = http.urlopen('GET', page_url)
@@ -51,21 +49,27 @@ for d in range(1,(number_of_pages+1)):
 
     #feed it into Beautiful Soup, our library for parsing html
     document_bs = BeautifulSoup(page_html, "lxml")
+   
     #find the table on the page we want based on its id
     documents_table = document_bs.find("table", {"id": "dttPubRecords"})
+   
     # find the tbody so we're ignoring rows in the header
     documents_table_body = documents_table.find("tbody")
+    
     # iterate through each row of the table
     for tr in documents_table_body.find_all("tr"):
-        logfile = open("foia-log.csv", "a")
+        logfile = open("foia-download-log.csv", "a")
 
         output_row = []
+       
+        #find all the link tags
         document_link = tr.find("a").get('href')
         document_name = tr.find("a").get_text()
 
         output_row.append(document_name)
         output_row.append(document_link)
 
+        #open the document url
         document_url = "https://foiaonline.regulations.gov{}".format(document_link)
         document_html = http.urlopen('GET', document_url)
         document_html = document_html.data
@@ -86,9 +90,9 @@ for d in range(1,(number_of_pages+1)):
             output_row.append("Downloaded successfully")
             counter += 1
         except:
-            output_row.append("Error on download")
+            output_row.append("Error on downloaded")
 
-        with open('foia-log.csv', 'a', newline='') as logfile:
+        with open('foia-download-log.csv', 'a', newline='') as logfile:
             writer = csv.writer(logfile)
             writer.writerow(output_row)
 
